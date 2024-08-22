f064c34f07410926137d6e95dbba4d221721010c
DY-2001 <dushyant.y20@iiits.in>
2024-08-22 20:06:44 +0530

    auth middleware added

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verified) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
