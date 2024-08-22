868315163b4469a91d8639a55eae943ba331a123
DY-2001 <dushyant.y20@iiits.in>
2024-08-22 23:39:09 +0530

    expense limit backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,12 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    expenseLimit: {
+      startDate: { type: Date, default: Date.now },
+      endDate: { type: Date },
+      limit: { type: Number },
+      amountSpent: { type: Number, default: 0 },
+    },
   },
   { timestamps: true }
 );
