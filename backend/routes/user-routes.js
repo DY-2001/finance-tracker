a215abdc57062f41231882a95b1e73cc4c17e606
DY-2001 <dushyant.y20@iiits.in>
2024-08-22 16:04:26 +0530

    user route created

---
+++ backend/routes/user-routes.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const { register } = require('../controllers/user-controller');
+
+router.post('/register', register);
