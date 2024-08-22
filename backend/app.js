e86e414b17f2ad275060bb986e4b6e6201af6479
DY-2001 <dushyant.y20@iiits.in>
2024-08-22 16:01:22 +0530

    database connection initiate

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express"); 
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use("/user", require("./routes/user-routes"))
+
+module.exports = app;
