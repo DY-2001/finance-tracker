9d63dd0eebd10580c1ed9a484257fd1a70c8ddf0
DY-2001 <dushyant.y20@iiits.in>
2024-08-22 19:05:29 +0530

    user authentication

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,3 +1,62 @@
-const register = async (req, res) => {};
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-module.exports = { register };
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ success: false, message: "User already exists" });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404).json({ success: false, message: "User not found" });
+      return;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      res.status(400).json({ success: false, message: "Invalid credentials" });
+      return;
+    }
+
+    const token = jwt.sign(
+      { email: user.email, id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res
+      .status(200)
+      .header("Authorization", token)
+      .json({
+        success: true,
+        message: "Login Success!",
+        token: token,
+        user: user,
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+module.exports = { register, login };
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/user-controller');
+const { register, login } = require('../controllers/user-controller');
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
